44197dc816f39586e49356886ae37eb937cb475a
Miriam Priscila Tafolla Astorga <dev5@clkseguros.com>
2025-11-06 10:26:18 -0600

    Prueba de cambio para git #2, #1

--- Controllers/ClinicaController.cs
+++ Controllers/ClinicaController.cs
@@ -14,6 +14,7 @@ namespace VeTLink.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
+    //Prueba de cambio para git
     public class ClinicaController(ApplicationDbContext context,
         UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IMapper mapper) : ControllerBase
     {
